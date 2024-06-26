2ca501775d7eb746379aa79366c1bd266635a81c
Muhamad Nouman Khan <mnoumanuaar@yahoo.com>
2024-06-26 14:45:39 +0400

    Add project files.

---
+++ FluentValidationTestApp/Controllers/ValidationtestController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FluentValidationTestApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValidationtestController : ControllerBase
+    {
+        // GET: api/<ValidationtestController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+        // POST api/<ValidationtestController>
+        [HttpPost]
+        public IActionResult TestValidation( string email, string name, int age)
+        {
+            var person = new Person(name, age, email);
+
+            var validator= new PersonValidator();
+            var result= validator.Validate(person);
+
+            if (!result.IsValid)
+            {
+                foreach (var error in result.Errors)
+                {
+                    Console.WriteLine(error.ErrorMessage);
+                    return Ok(error.ErrorMessage);
+                }
+            }
+            Console.WriteLine("Finished");
+            Console.WriteLine();
+            return Ok("Finished");
+
+
+
+
+        }
+
+
+    }
+}
---
+++ FluentValidationTestApp/Person.cs
@@ -0,0 +1,40 @@
+﻿using FluentValidation;
+using FluentValidation.Validators;
+
+namespace FluentValidationTestApp
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+
+        public Person(string name, int age, string email)
+        {
+            Name = name;
+            Age = age;
+            Email = email;
+        }
+
+
+    }
+
+    public class PersonValidator : AbstractValidator<Person>
+    {
+        public PersonValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .NotNull().NotEmpty()
+                .WithMessage("Name is required");
+
+            RuleFor(x => x.Age)
+                .NotNull().NotEmpty().InclusiveBetween(0, 100)
+                .WithMessage("Age must be between 1 and 100");
+
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Email address is required")
+                      .EmailAddress().WithMessage("A valid email is required");
+        }
+
+    }
+}
